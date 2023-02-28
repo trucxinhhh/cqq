5016897acc8245a1326bab567da7793e965fb025
trucxinhhh <93142476+trucxinhhh@users.noreply.github.com>
2023-02-28 14:28:03 +0700

    aaaaaaaaaaaaaa

--- fastAPI.py
+++ fastAPI.py
@@ -1,23 +1,104 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import Form, Request, FastAPI , Depends, HTTPException,status 
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+from pydantic import BaseModel
+from typing import Union
+from jose import JWTError, jwt
+from passlib.context import CryptContext 
 
-templates = Jinja2Templates(directory="templates")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token") 
+fake_users_db = {
+    "truc": {
+        "username": "truc",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashed123456",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 app = FastAPI()
-users = [
-    {'username': 'truc', 'password': '123456'},
-    {'username': 'test', 'password': '123456'},
-]
-@app.post("/login")
-async def login(request: Request, username: str = Form(...), password: str = Form(...)):
-    if username == "truc" and password == "123456":
-         return templates.TemplateResponse("Interface.html", {"request": request})
-    else:
-        return {"message": "Invalid username or password"}
+
+templates = Jinja2Templates(directory="templates")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+   
+
+class UserInDB(User):
+    hashed_password: str = Form(...), 
+
+
+
+def fake_hash_password(password: str = Form(...)):
+    return "fakehashed" + password
+
+def fake_decode_token(username : str = Form(...)):
+    return User(
+        username=username + "fakedecoded",
+        email="john@example.com"
+    )
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token",response_class=HTMLResponse)
+async def login(request: Request, form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    return templates.TemplateResponse("test.html", {"request": request})
+
+# @app.get("/users/me")
+# async def read_users_me(current_user: User = Depends(get_current_active_user)):
+#     return current_user
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
-    return templates.TemplateResponse("test.html", {"request": request})
+    return templates.TemplateResponse("new.html", {"request": request})
+
+
